2c2705b106c2c2843453f76ca0bdeb26a563de52
DublinJim <59180380+DublinJim@users.noreply.github.com>
2022-11-23 15:18:04 -0500

    adds a second popup

--- src/main/java/com/example/cmdlinedemo/HelloController.java
+++ src/main/java/com/example/cmdlinedemo/HelloController.java
@@ -5,7 +5,6 @@ import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Popup;
@@ -23,6 +22,10 @@ public class HelloController implements Initializable {
     private String[] cmdArray;
     private Runtime runtime;
     private Process process;
+    private Popup popup;
+    private Label popupLbl;
+    private Label popupLbl2;
+    private Stage stage;
 
     public void dosCommand() throws IOException {
 
@@ -33,7 +36,7 @@ public class HelloController implements Initializable {
 
 
         Arrays.stream(cmdArray).forEach(System.out::println);
-        runtime = Runtime.getRuntime();
+
         process = runtime.exec(cmdArray);
         txt1.setVisible(true);
         txt1.setText("Notepad opens and a new file " + cmdArray[1] + " is created ");
@@ -43,29 +46,45 @@ public class HelloController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        stage = HelloApplication.myStage;
         txt1.setVisible(false);
 
-
+        popup = new Popup();
+        runtime = Runtime.getRuntime();
     }
 
     public void popUpWindow(ActionEvent actionEvent) {
-        Popup popup = new Popup();
-        Label popupLbl = new Label("Warning");
-        popupLbl.setPadding(new Insets(10.0));
 
-        Stage stage = HelloApplication.myStage;
+
+        popupLbl = new Label("Warning");
+        popupLbl.setPadding(new Insets(10.0));
         Button noBtn = new Button("Option 1");
         Button yesBtn = new Button("Option 2");
-         noBtn.setPadding(new Insets(10));
-         yesBtn.setPadding(new Insets(10));
+
+        noBtn.setPadding(new Insets(10));
+        yesBtn.setPadding(new Insets(10));
         popupLbl.setMinWidth(50);
         popupLbl.setMinHeight(50);
         popupLbl.setStyle("-fx-background-color: blue;");
 
-        HBox optContainer = new HBox(noBtn,yesBtn,popupLbl);
-        VBox optContainer2 = new VBox(noBtn,yesBtn,popupLbl);
-        popup.getContent().add(optContainer2);
-
+        VBox optContainer = new VBox(noBtn, yesBtn, popupLbl);
+        popup.getContent().add(optContainer);
+        stage = HelloApplication.myStage;
         popup.show(stage);
+    }
+
+    public void popUpWindowVbox(ActionEvent actionEvent) {
+//here we go
+        Popup popup2 = new Popup();
+        popupLbl2 = new Label("Second label");
+
+        Button secondPopUpButton = new Button("new button");
+        secondPopUpButton.setPadding(new Insets(10));
+        VBox optContainer = new VBox(secondPopUpButton,popupLbl2);
+        popup2.getContent().add(optContainer);
+
+        stage = HelloApplication.myStage;
+        popup2.show(stage);
+        System.out.println("done");
     }
 }
