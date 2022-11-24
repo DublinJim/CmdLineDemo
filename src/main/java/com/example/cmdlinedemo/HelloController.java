05e6d27b26734fcb1040822816aaaec79aeeb3f6
DublinJim <59180380+DublinJim@users.noreply.github.com>
2022-11-24 13:22:04 -0500

    adds a third popup

--- src/main/java/com/example/cmdlinedemo/HelloController.java
+++ src/main/java/com/example/cmdlinedemo/HelloController.java
@@ -2,7 +2,6 @@ package com.example.cmdlinedemo;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
@@ -14,6 +13,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Popup;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.net.URL;
@@ -22,12 +22,11 @@ import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
 
+    public static Button btnCancel;
     private static Stage popStage;
     public Button Btn1;
     public Text txt1;
     public Button btnRestart;
-    public static Button btnCancel;
-
     private String[] cmdArray;
     private Runtime runtime;
     private Process process;
@@ -40,13 +39,16 @@ public class HelloController implements Initializable {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("pop.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         popStage = new Stage();
+        popStage.initStyle(StageStyle.UNDECORATED);
         popStage.setTitle("popup");
         popStage.setScene(scene);
         popStage.show();
 
-
     }
+private static void loadCheck()
+{
 
+}
     public void dosCommand() throws IOException {
 
 
@@ -54,9 +56,7 @@ public class HelloController implements Initializable {
         cmdArray[0] = "notepad.exe";
         cmdArray[1] = "NewText.txt";
 
-
         Arrays.stream(cmdArray).forEach(System.out::println);
-
         process = runtime.exec(cmdArray);
         txt1.setVisible(true);
         txt1.setText("Notepad opens and a new file " + cmdArray[1] + " is created ");
@@ -66,9 +66,8 @@ public class HelloController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        System.out.println("first");
-        stage = HelloApplication.myStage;
 
+        stage = HelloApplication.myStage;
         popupLbl = new Label("Warning");
         popup = new Popup();
         runtime = Runtime.getRuntime();
@@ -80,15 +79,12 @@ public class HelloController implements Initializable {
         popupLbl.setPadding(new Insets(10.0));
         Button noBtn = new Button("Close");
         Button yesBtn = new Button("Option 2");
-
         noBtn.setPadding(new Insets(10));
         yesBtn.setPadding(new Insets(10));
         popupLbl.setMinWidth(50);
         popupLbl.setMinHeight(50);
         popupLbl.setStyle("-fx-background-color: gray;");
-
         HBox optContainer = new HBox(noBtn, yesBtn, popupLbl);
-
         optContainer.setPrefSize(880, 220);
         optContainer.setPadding(new Insets(10));
         optContainer.setSpacing(10.0);
@@ -127,12 +123,8 @@ public class HelloController implements Initializable {
     }
 
     public void cancelAll(ActionEvent actionEvent) {
-popStage.hide();
+        popStage.hide();
 
-    }
-
-    public void shutTheMainStage() {
-        HelloApplication.myStage.hide();
     }
 
 }
